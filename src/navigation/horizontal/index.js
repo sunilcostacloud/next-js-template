ae325042aa71a0acb359790652f6fda7e93df7d1
sunil <sunil@costacloud.com>
2023-12-13 14:03:28 +0530

    update

---
+++ src/navigation/horizontal/index.js
@@ -0,0 +1,19 @@
+const navigation = () => [
+  {
+    title: 'Home',
+    path: '/home',
+    icon: 'tabler:smart-home'
+  },
+  {
+    title: 'Second Page',
+    path: '/second-page',
+    icon: 'tabler:mail'
+  },
+  {
+    path: '/acl',
+    title: 'Access Control',
+    icon: 'tabler:shield'
+  }
+]
+
+export default navigation
